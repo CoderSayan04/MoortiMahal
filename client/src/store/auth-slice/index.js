9c8860a5af173d619c911970a7fa9983fc072792
Sayan Mitra <sayanmitra105@gmail.com>
2025-07-07 15:57:17 +0530

    bug fixed

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -12,7 +12,7 @@ export const registerUser = createAsyncThunk(
   "/auth/register",
   async (formData) => {
     const response = await axios.post(
-      "https://murtimahal-1.onrender.com/api/auth/register",
+      "http://localhost:5000/api/auth/register",
       formData,
       {
         withCredentials: true,
@@ -27,7 +27,7 @@ export const loginUser = createAsyncThunk(
   "/auth/login",
   async (formData) => {
     const response = await axios.post(
-      "https://murtimahal-1.onrender.com/api/auth/login",
+      "http://localhost:5000/api/auth/login",
       formData,
       {
         withCredentials: true,
@@ -42,7 +42,7 @@ export const logoutUser = createAsyncThunk(
   "/auth/logout",
   async () => {
     const response = await axios.post(
-      "https://murtimahal-1.onrender.com/api/auth/logout",
+      "http://localhost:5000/api/auth/logout",
       {},
       {
         withCredentials: true,
@@ -57,7 +57,7 @@ export const checkAuth = createAsyncThunk(
   "/auth/checkauth",
   async (token) => {
     const response = await axios.get(
-      "https://murtimahal-1.onrender.com/api/auth/check-auth",
+      "http://localhost:5000/api/auth/check-auth",
       {
         withCredentials: true,
         headers: {
