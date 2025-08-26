6496adf18a93f9057c183b3d502daa188b1306f9
Sayan Mitra <sayanmitra105@gmail.com>
2025-08-26 10:11:01 +0530

    Home page updated

--- client/src/pages/shopping-view/home.jsx
+++ client/src/pages/shopping-view/home.jsx
@@ -1,8 +1,8 @@
 import { Button } from "@/components/ui/button";
 import React from "react";
 import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
+   ChevronLeft,
+  ChevronRight,
   Church,
   Crown,
   Flame,
